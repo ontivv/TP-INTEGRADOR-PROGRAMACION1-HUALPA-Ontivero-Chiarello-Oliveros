190c7690d4b473f80553250b62ba3eb3668efea7
Joaquin120 <joaquinchiarello28@gmail.com>
2025-11-04 00:44:25 +0100

    Create validaciones.py
    subo modulo de validaciones

---
+++ validaciones.py
@@ -0,0 +1,59 @@
+#numeros
+def ventero(): #solo permite enteros
+    try:
+        if isinstance(x,str):
+            return False
+        elif x % 1 == 0:
+            return True
+        else:
+            print (0/0)
+    except ZeroDivisionError:
+        return False
+
+def vpositivo(x): #solo permite numeros naturales (1:infinito)
+    try:
+        if isinstance(x,str):
+            return False
+        elif x % 1 == 0 and not x <= 0:
+            return True
+        else:
+            print (0/0)
+    except ZeroDivisionError:
+        return False
+
+#######      .isspace()      verifica si solo hay espacio en blanco por ejemplo   "     " = True, "hola  " = False
+def vfloat(x):   #valida numeros con decimales
+    try:
+        if isinstance(x,str):
+            return False
+        elif x % 1 != 0:
+            return True
+        else:
+            print (0/0)
+    except ZeroDivisionError:
+        return False
+
+
+def producto_precio(nombre,precio):  #valida el nombre y precio que debe ser en decimales
+    if nombre.isspace():
+        return False
+    else:
+        nombre = True
+    if nombre == True:
+        if vfloat(precio) == False:
+            return False
+        else:
+            return True
+
+
+def producto_catidad(nombre,precio):  #valida el nombre y precio que debe ser entero
+    if nombre.isspace():
+        return False
+    else:
+        nombre = True
+    if nombre == True:
+        if vpositivo(precio) == False:
+            return False
+        else:
+            return True
+
